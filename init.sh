c72a2d580082443a36f87df4dd1ce41cb24b41ad
Cat-In-Coat <rodion2023@mail.ru>
2020-11-01 17:08:19 +0400

    Here we go again

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/defult
 sudo /etc/init.d/nginx restart
+cd /home/box/web
+gunicorn -b 0.0.0.0:8080 hello:app
