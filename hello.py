15eea26a2267ce0b46a087166838828230db5e54
Cat-In-Coat <rodion2023@mail.ru>
2020-11-01 18:11:10 +0400

    MAybe final commit

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 def app(env, start):
-	data = [bytes(i +'?n', 'ascii') for i in env['QUERY_STRRING'].split('&')]
+	data = [i +'/n' for i in env['QUERY_STRING'].split('&')]
+	print("".join(data))
+	for i in range(len(data)):
+		data[i] = bytes(data[i], 'ascii')
 	headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(data)))]
 	start('200 OK', headers)
 	return data
