4a1a684c183424ca020a15cf357eaecb616b154f
Cat-In-Coat <rodion2023@mail.ru>
2020-11-01 18:35:01 +0400

    Final commit

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
-def app(env, start):
-	data = [i +'\n' for i in env['QUERY_STRING'].split('&')]
-	print(env['QUERY_STRING'])
-	print("".join(data))
-	for i in range(len(data)):
-		data[i] = bytes(data[i], 'ascii')
-	headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(data)))]
-	start('200 OK', headers)
-	return data
+def app(environ, start_responce):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    data = environ['QUERY_STRING'].split('&')
+    body = '\n'.join(data).encode('utf-8')
+    start_responce(status, headers)
+    return [body]
