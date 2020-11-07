b81815d42f018686b7e0bbc121eed99b7e40a697
Cat-In-Coat <rodion2023@mail.ru>
2020-11-07 18:14:37 +0400

    New little changes

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt-get install python3
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade django
-sudo pip3 install -- upgrade gunicorn
+virtualenv -p python3 myvenv
+source myvenv/bin/activate
+pip install --upgrade pip
+pip install django
+pip install gunicorn
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/defult
 sudo /etc/init.d/nginx restart
