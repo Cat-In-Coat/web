0e1493d73ad45be87abc56fd16aedbc9fff23d5c
Cat-In-Coat <rodion2023@mail.ru>
2020-11-07 19:52:20 +0400

    New little changes

--- init.sh
+++ init.sh
@@ -10,10 +10,9 @@ cp /home/box/web/etc/gunicorn*.conf   /etc/gunicorn.d/
 cp edinorogs/gunicorn-debian /usr/sbin/
 cp edinorogs/gunicorn /usr/bin/
 cp edinorogs/gunicorn_* /usr/bin/
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/defult
 sudo /etc/init.d/nginx restart
 cd /home/box/web/ask/ask
-gunicorn -b 0.0.0.0:8080 wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
