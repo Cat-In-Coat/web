9aee243fa9bd213048969262de455a49a97f82bf
Cat-In-Coat <rodion2023@mail.ru>
2020-11-07 19:46:38 +0400

    New little changes

--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
-virtualenv -p python3 myvenv
-source myvenv/bin/activate
-pip install --upgrade pip
+apt-get update
+apt-get install -f python3.5
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python3.5 get-pip.py
 pip install django
 pip install gunicorn
+rm /etc/nginx/sites-enabled/default
+cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+cp /home/box/web/etc/gunicorn*.conf   /etc/gunicorn.d/
+cp edinorogs/gunicorn-debian /usr/sbin/
+cp edinorogs/gunicorn /usr/bin/
+cp edinorogs/gunicorn_* /usr/bin/
+sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/defult
 sudo /etc/init.d/nginx restart
