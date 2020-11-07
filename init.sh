3db1a83fe0299a1099c49535000d28c39964bb9f
Cat-In-Coat <rodion2023@mail.ru>
2020-11-07 21:13:57 +0400

    New little changes

--- init.sh
+++ init.sh
@@ -15,4 +15,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/defult
 sudo /etc/init.d/nginx restart
 cd /home/box/web/ask/ask
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf wsgi:application
