1e29761bb276f3c8d8edac935a598be00236127b
Cat-In-Coat <rodion2023@mail.ru>
2020-11-09 20:50:43 +0400

    It worked!!!

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ..qa import views  # fixme
+from qa import views  # fixme
 
 
 urlpatterns = [
