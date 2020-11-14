46a02cb3428f9d417c6ac3db9f0347b728ba847c
Cat-In-Coat <rodion2023@mail.ru>
2020-11-14 20:30:29 +0400

    Attempt 2

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
-sudo apt-get install -f python3.5
+sudo apt-get install -f python3.8
 sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-sudo python3.5 get-pip.py
-pip install django
-pip install gunicorn
+sudo python3.8 get-pip.py
+sudo pip install django
+sudo pip install gunicorn
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/defult
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
