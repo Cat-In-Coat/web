0487ebce46344258fb3b1f381de711803a2761ad
Cat-In-Coat <rodion2023@mail.ru>
2020-11-21 19:59:42 +0400

    Attempt 9

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get install -f python3.5
 sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo python3.8.3 get-pip.py
-sudo pip3 install django
-sudo pip3 install gunicorn
+sudo pip3 install django3.1.3
+sudo pip3 install gunicorn20.0.4
 sudo pip3 install pysqlite3
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/defult
