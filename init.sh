2f5ac8564905d693462963360da756d3991a67d7
Cat-In-Coat <rodion2023@mail.ru>
2020-11-22 20:45:56 +0400

    Attempt 10

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -f python3.5
+sudo apt-get install -f python3.8.3
 sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo python3.8.3 get-pip.py
 sudo pip3 install django3.1.3
@@ -11,4 +11,4 @@ sudo chmod -R a+rwx /home/box/web
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 cd /home/box/web/ask
-gunicorn --config=/home/box/web/etc/gunicorn-django.conf -b 0.0.0.0:8000 ask.wsgi
+python3.8.3 gunicorn --config=/home/box/web/etc/gunicorn-django.conf -b 0.0.0.0:8000 ask.wsgi
